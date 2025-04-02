d0bf9076b9c96a3c0907e4af213f11c464897475
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-02 05:31:34 -0300

    feat: update TypeScript configuration and add node-specific tsconfig
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 import App from './App.tsx'
 
 const container = document.getElementById('root')
-const root = createRoot(container)
+const root = createRoot(container!)
 
 root.render(
     <StrictMode>
