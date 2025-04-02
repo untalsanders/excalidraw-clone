55c51e535237965ff664ba94f4d27c820dc3ffb0
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-02 05:21:57 -0300

    feat: migrate project to TypeScript and update configuration files
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,24 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tsEslint from 'typescript-eslint'
 
-export default [
+export default defineConfig([
     { ignores: ['dist'] },
     {
-        files: ['**/*.{js,jsx}'],
+        extends: [js.configs.recommended, ...tsEslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
-            parserOptions: {
-                ecmaVersion: 'latest',
-                ecmaFeatures: {
-                    jsx: true,
-                },
-                sourceType: 'module',
-            },
         },
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
         },
         rules: {
-            ...js.configs.recommended.rules,
             ...reactHooks.configs.recommended.rules,
             'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
             'react-refresh/only-export-components': [
@@ -34,4 +29,4 @@ export default [
             ],
         },
     },
-]
+])
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
