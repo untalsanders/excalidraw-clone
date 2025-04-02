0cd276c02c64e915e9fced052d082d3f08551abe
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-02 04:19:24 -0300

    feat: add ESLint configuration and update project files for Excalidraw clone
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from '/logo.svg'
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Vite + React + SWC</h1>
+            <img src={logo} alt="Excalidraw logo" />
+            <p>Excalidraw Clone</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-
-function App() {
-    return (
-        <div className="App">
-            <h1>Vite + React + SWC</h1>
-            <img src="/logo.svg" alt="logo" />
-            <p>Excalidraw Clone</p>
-        </div>
-    )
-}
 
 root.render(
     <StrictMode>
